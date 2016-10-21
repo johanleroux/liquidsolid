0c463689972c6094a7ad7eadfc232ae987539f8e
Johan le Roux <jleroux99@gmail.com>
2016-10-21 10:04:52 +0200

    Template commit

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,11 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.sass('app.scss', 'resources/assets/css/app.css')
+       .webpack('app.js')
+       .styles([
+         'app.css',
+         'main.css',
+         'responsive.css',
+       ]);
 });
