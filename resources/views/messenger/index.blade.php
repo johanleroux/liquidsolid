348eccc0ec7fe8a774035e5b5e504017c283bf01
FerreiraX <xanderferreira@yahoo.com>
2016-10-22 14:33:36 +0200

    Added PM System

---
+++ resources/views/messenger/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.default')
+
+@section('content')
+  @if (Session::has('error_message'))
+      <div class="alert alert-danger" role="alert">
+          {{ Session::get('error_message') }}
+      </div>
+  @endif
+  @if(Session::has('flash_message'))
+      <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
+  @endif
+  @if($threads->count() > 0)
+  <div style="margin-right:100px;margin-left: 100px">
+      <table class="table striped" style="width:100%">
+          <tr class="header">
+              <th>Thread</th>
+              <th>Message</th>
+              <th>Creator</th>
+              <th>Participants</th>
+          </tr>
+          @foreach($threads as $thread)
+            @if($thread->creator()->id == Auth::id())
+            <tr class="header">
+                <td>{!! link_to('messages/' . $thread->id, $thread->subject) !!}</td>
+                <td>{{ $thread->latestMessage->body }}</td>
+                <td>{{ $thread->creator()->name }}</td>
+                <td>{{ $thread->participantsString(Auth::id()) }}</td>
+            </tr>
+            @endif
+          @endforeach
+
+      </table>
+    </div>
+  @else
+      <p>No threads found</p>
+  @endif
+@endsection
